33cf2a4e281cc0e74650713cf8ca5af457f64a23
maksloposha <maksloposha@gmail.com>
2022-07-23 23:20:27 +0300

    Create Shopping.java

---
+++ intelliasTest/src/main/java/com/example/intelliastest/Shopping.java
@@ -0,0 +1,27 @@
+package com.example.intelliastest;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Shopping {
+
+    @FXML
+    private TextField UserNameField;
+
+    @FXML
+    private Button enterIdProductButton;
+
+    @FXML
+    private TextField productIdField;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    private TextField userIdField;
+
+    @FXML
+    private TextField userMoneyAmount;
+
+}
